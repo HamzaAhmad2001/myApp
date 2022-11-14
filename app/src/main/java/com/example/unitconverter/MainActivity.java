01a9792169e1a27e83f94f8d6d9853d9a68cd85d
alarrabee <hamza.ibrahem@hotmail.com>
2022-11-14 15:51:02 +0200

    hello this is first commit in this poroject

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.unitconverter;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    public double ConvertToPound(double k)
+    {
+        return k*2.205;
+    }
+
+
+
+
+}
